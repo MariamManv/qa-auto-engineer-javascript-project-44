7262bf350f8ea0d2de9dd1809604eb8ff1166f54
MariamManv <155296293+MariamManv@users.noreply.github.com>
2024-07-15 20:24:05 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 
-import * as engine from './src/index.js';
-import * as brainEven from './src/even.js';
+import * as engine from '../src/index.js';
+import * as brainEven from '../src/even.js';
 
 (function main() {
     engine.executeBrainGames(brainEven);
