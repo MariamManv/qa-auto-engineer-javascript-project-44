a9f6927918ba85935b7b93e0ece4d83384e47177
MariamManv <155296293+MariamManv@users.noreply.github.com>
2024-07-16 12:52:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ export const executeBrainGames = (specificGame) => {
                 console.log(`Congratulations, ${userName}`)
             }
         } else {
-            console.log(`${userInput} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+            console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
             break
         }
 
