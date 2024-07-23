a7017a47b5d6e9ae00fb8231ec59838dff1906c5
MariamManv <155296293+MariamManv@users.noreply.github.com>
2024-07-23 12:55:41 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const minNumber = 1;
 export const maxNumber = 1000;
 export const getRandomNumber = () => {
-  Math.floor((Math.random() * (maxNumber - minNumber + 1)) + minNumber);
+  return Math.floor((Math.random() * (maxNumber - minNumber + 1)) + minNumber);
 };
