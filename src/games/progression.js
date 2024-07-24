47271c406d1a1097089f4e7e1ab4311fa74ccfc2
MariamManv <155296293+MariamManv@users.noreply.github.com>
2024-07-24 14:02:40 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,7 +6,7 @@ export const minCommonDifferenceNumber = 1;
 export const maxCommonDifferenceNumber = 15;
 export const progressionLength = 10;
 
-export const makeprogression = () => {
+export const makeProgression = () => {
   const initialTerm = getRandomNumber();
   const commonDifference = Math.floor(Math.random() * (
     maxCommonDifferenceNumber - minCommonDifferenceNumber + 1))
@@ -17,10 +17,11 @@ export const makeprogression = () => {
     progression.push(initialTerm + i * commonDifference);
     i += 1;
   }
+  return progression
 };
 
 export const getQuestionAndCorrectAnswer = () => {
-  const newProgression = makeprogression();
+  const newProgression = makeProgression();
   const correctIndex = Math.floor(Math.random() * progressionLength);
   const correctAnswer = String(newProgression[correctIndex]);
   newProgression[correctIndex] = '..';
