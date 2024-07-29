d2176bf718a2d83950babe66ae99222e19d52e62
MariamManv <155296293+MariamManv@users.noreply.github.com>
2024-07-29 19:03:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,19 @@ export const executeBrainGames = (specificGame) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(specificGame.rulesOfTheGame);
-  let correctAnswerCount = 0;
+
   for (let count = 0; count < roundNumber; count += 1) {
     const { question, correctAnswer } = specificGame.getQuestionAndCorrectAnswer();
     console.log(`Question: ${question}`);
     const userInput = readlineSync.question('Your answer: ');
-    if (userInput === correctAnswer) {
-      console.log('Correct!');
-      correctAnswerCount += 1;
-      if (correctAnswerCount === roundNumber) {
-        console.log(`Congratulations, ${userName}!`);
-      }
-    } else {
-      console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-      break;
+
+    if (userInput !== correctAnswer) {
+      console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
+
+  console.log(`Congratulations, ${userName}!`);
 };
